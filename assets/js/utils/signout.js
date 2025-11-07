eeff88fabf028ae2b446f6319ae9c398559ee5ac
art group of companies <artleasing2025@gmail.com>
2025-11-07 22:11:16 +0530

    all file delete

--- assets/js/utils/signout.js
+++
@@ -1,5 +0,0 @@
-// ---------------- SIGN OUT ----------------
-document.getElementById("signOutBtn").addEventListener("click", () => {
-  localStorage.removeItem("loggedUser");
-  window.location.href = "./";
-});
