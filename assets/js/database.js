e34897b5c8af19db851915e53269135640e3d375
art group of companies <artleasing2025@gmail.com>
2025-12-02 09:53:56 +0530

    db path

--- assets/js/database.js
+++ assets/js/database.js
@@ -4,24 +4,13 @@ import { getFirestore,  getDoc,  doc,  addDoc,  updateDoc, deleteDoc,  collectio
 
 const firebaseConfig = {
     // main db
-    /*
    apiKey: "AIzaSyCOcsL9ahdFUunWDAcv5JZdnMxMZehquhg",
     authDomain: "art-staffmanage.firebaseapp.com",
     projectId: "art-staffmanage",
     storageBucket: "art-staffmanage.firebasestorage.app",
     messagingSenderId: "951269778971",
     appId: "1:951269778971:web:11fbb26385588ced133abe" 
--------------------- */
 
- //test db
-    apiKey: "AIzaSyArh9Lqdyio6QZ276L2ciMwEgU8BgigrFs",
-    authDomain: "user-atten.firebaseapp.com",
-    databaseURL: "https://user-atten-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "user-atten",
-    storageBucket: "user-atten.firebasestorage.app",
-    messagingSenderId: "13092297593",
-    appId: "1:13092297593:web:dd99a876e5dec1714fada8"
-   
 };
 
 const app = initializeApp(firebaseConfig);
